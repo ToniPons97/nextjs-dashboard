41835ba5666903265632c70746027af4d8d2fb02
Antonio Pons <toninopons@gmail.com>
2024-01-29 14:19:32 +0100

    Implemented pagination.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import page from '../customers/page';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 type InvoicesPageProps = {
   searchParams?: {
@@ -17,7 +18,9 @@ type InvoicesPageProps = {
 export default async function Page({ searchParams }: InvoicesPageProps) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  
+
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -31,7 +34,7 @@ export default async function Page({ searchParams }: InvoicesPageProps) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
